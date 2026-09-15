7a9a5031b6ca7f3ceae40a1436b7469a27045d3b
AbdulBaseer7896 <abdulbasirqazi7896@gmail.com>
2026-09-15 21:49:43 +0500

    v2.1) Now try to deployed on the Digital OCean

--- app.js
+++ app.js
@@ -13,14 +13,18 @@ const app = express();
 // const cors = require('cors');
 // app.use(cors());
 // app.use(cors());
-app.use(cors({
-  // origin: ['https://freightflowsolutions.co','https://dispatchingllc.netlify.app' , 'https://freightflowlogistics.netlify.app'], // Your React app's origin
-  // origin: 'https://freightflowlogistics.netlify.app', // Your React app's origin
-  origin: 'https://freightflowlogistic.com', // Your React app's origin
-  // origin: 'http://localhost:3000', // Your React app's origin
-  // credentials: true
-}));
+// app.use(cors({
+//   // origin: ['https://freightflowsolutions.co','https://dispatchingllc.netlify.app' , 'https://freightflowlogistics.netlify.app'], // Your React app's origin
+//   // origin: 'https://freightflowlogistics.netlify.app', // Your React app's origin
+//   origin: 'https://freightflowlogistic.com', // Your React app's origin
+//   // origin: 'http://localhost:3000', // Your React app's origin
+//   // credentials: true
+// }));
 
+
+app.use(cors({
+  origin: ['https://freightflowlogistic.com', 'http://localhost:3000'], // add whatever origin the frontend runs on
+}));
 
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
